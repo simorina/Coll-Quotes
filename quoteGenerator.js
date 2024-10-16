0f597a33ae68a952f06495e5444c69b2192076f1
simorina <rinaldisimone8@gmail.com>
2024-10-16 13:05:14 +0200

    update with fetch

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,11 +1,13 @@
 
 
-import data from './quotes.json' assert { type: "json" };
+
 var link = document.createElement('link');
 // set the attributes for link element
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = '../style.css';
+
+
 
 
 let q = document.getElementById("quote")
@@ -15,16 +17,31 @@ let a = document.getElementById("author")
 
 // q.innerHTML = data.quotes[rnd].quote 
 // a.innerHTML = "~ " + data.quotes[rnd].author
-
-
-function loadQuote(){
-    let rnd = Math.floor(Math.random() * data.quotes.length); 
-    q.animate
-    q.innerHTML = data.quotes[rnd].quote 
-    a.innerHTML = "~ " + data.quotes[rnd].author
+let data;
+fetch('https://raw.githubusercontent.com/AtaGowani/daily-motivation/refs/heads/master/src/data/quotes.json')
+.then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log(data);
     
-}
-setInterval(loadQuote,5000)
+    function loadQuote(){
+        let rnd = Math.floor(Math.random() * data.length); 
+        q.animate
+        q.innerHTML = data[rnd].quote 
+        a.innerHTML = "~ " + data[rnd].author
+        
+    }
+    setInterval(loadQuote,5000)
+})
+.catch(function () {
+    this.dataError = true;
+})
+
+
 
 
 
